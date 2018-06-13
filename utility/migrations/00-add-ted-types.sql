8161519d1d3da3cc2ac9af14ba3e32f662a34a10
Jacek Rzeniewicz <jacek@boclips.com>
2018-06-13 12:43:53 +0100

    [#157931736] Delete migration files, the queries are in init scripts

--- utility/migrations/00-add-ted-types.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO type (id, name, displayOrder) VALUES (10, 'TED Talks', 10);
-INSERT INTO type (id, name, displayOrder) VALUES (11, 'TED-Ed', 11);
-
-UPDATE metadata_orig SET type_id = 10 WHERE source = 'TED Talks';
-UPDATE metadata_orig SET type_id = 11 WHERE source = 'TED-Ed';
-UPDATE metadata_orig SET type_id = 9 WHERE source = 'The School of Life';
