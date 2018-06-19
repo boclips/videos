441c843057fb2af829b44941c27909b1dd80e930
Benjamin Gröhbiel <ben@boclips.com>
2018-06-19 15:55:33 -0500

    [#157550390] Fix gradle tasks for test and testE2E
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

--- video-cleanser/src/test/kotlin/com/boclips/cleanser/infrastructure/kaltura/PagedKalturaMediaServiceTest.kt
+++ video-cleanser/src/test/kotlin/com/boclips/cleanser/infrastructure/kaltura/PagedKalturaMediaServiceTest.kt
@@ -2,21 +2,19 @@ package com.boclips.cleanser.infrastructure.kaltura
 
 import com.boclips.cleanser.domain.model.MediaItem
 import com.boclips.cleanser.infrastructure.kaltura.client.KalturaMediaClient
+import com.nhaarman.mockito_kotlin.any
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.mockito.Mockito
 
 class PagedKalturaMediaServiceTest {
-    lateinit var pagedKalturaMediaService: PagedKalturaMediaService
-
     @Test
     fun getReadyMediaEntries_parsesMediaItems() {
         val mockKalturaClient = Mockito.mock(KalturaMediaClient::class.java)
-        pagedKalturaMediaService = PagedKalturaMediaService(mockKalturaClient)
-        Mockito.`when`(mockKalturaClient.fetch(500, 0))
+        Mockito.`when`(mockKalturaClient.fetch(any(), any(), any()))
                 .thenReturn(listOf(MediaItem(referenceId = "1"), MediaItem(referenceId = "2")))
 
-        val mediaItems = pagedKalturaMediaService.getReadyMediaEntries()
+        val mediaItems = PagedKalturaMediaService(mockKalturaClient).getReadyMediaEntries()
 
         assertThat(mediaItems).containsExactly(MediaItem(referenceId = "1"), MediaItem(referenceId = "2"))
     }
