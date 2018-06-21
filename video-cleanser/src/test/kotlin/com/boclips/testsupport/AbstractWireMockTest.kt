7dcdefb860c5304d06ec66558d184340f8145350
Benjamin Gröhbiel <ben@boclips.com>
2018-06-21 13:50:20 -0500

    [#157550390] Retrofit integration tests for Video Analysis Service
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

---
+++ video-cleanser/src/test/kotlin/com/boclips/testsupport/AbstractWireMockTest.kt
@@ -0,0 +1,25 @@
+package com.boclips.testsupport
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import org.junit.After
+import org.junit.Before
+
+abstract class AbstractWireMockTest {
+    companion object {
+        const val PORT = 8089
+    }
+
+    val wireMockServer = WireMockServer(WireMockConfiguration.wireMockConfig().port(PORT))
+
+    @Before
+    fun startAndResetWireMock() {
+        wireMockServer.start()
+        wireMockServer.resetAll()
+    }
+
+    @After
+    fun stopWireMock() {
+        wireMockServer.stop()
+    }
+}
