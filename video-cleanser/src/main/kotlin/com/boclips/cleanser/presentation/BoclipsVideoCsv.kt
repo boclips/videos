9a9c60453ff7210f104ce5f626eca491b3efaea7
Benjamin Gröhbiel <ben@boclips.com>
2018-06-21 17:35:27 -0500

    [#157550390] Add functionality to write to CSV file
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

---
+++ video-cleanser/src/main/kotlin/com/boclips/cleanser/presentation/BoclipsVideoCsv.kt
@@ -0,0 +1,8 @@
+package com.boclips.cleanser.presentation
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class BoclipsVideoCsv {
+    @JsonProperty(value = "Id")
+    var id: String = ""
+}
--- video-cleanser/src/main/kotlin/com/boclips/cleanser/presentation/UserShell.kt
+++ video-cleanser/src/main/kotlin/com/boclips/cleanser/presentation/UserShell.kt
@@ -1,43 +1,36 @@
 package com.boclips.cleanser.presentation
 
-import com.boclips.cleanser.domain.service.VideoAnalysisService
+import com.boclips.cleanser.application.CreateReport
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 import org.springframework.shell.standard.ShellOption
 
 @ShellComponent
-class UserShell(private val videoAnalysisService: VideoAnalysisService) {
+class UserShell(private val createReport: CreateReport) {
 
     @ShellMethod("Generate report with all unplayable videos")
     fun unplayableVideos(@ShellOption(help = "Specify filename") filename: String) {
         println("Fetching unplayable videos...")
-        val unplayableVideos = videoAnalysisService.getUnplayableVideos()
-        println("Generated file $filename with ${unplayableVideos.size} unplayable videos")
+        createReport.unplayableVideos(filename)
+        println("Finished job fetching unplayable videos")
     }
 
     @ShellMethod("Generate report with all playable videos")
     fun playableVideos(@ShellOption(help = "Please specify file name") filename: String) {
         println("Fetching all playable videos...")
-        val playableVideos = videoAnalysisService.getPlayableVideos()
-        println("Generated file $filename with ${playableVideos.size} unplayable videos")
+        createReport.playableVideos(filename)
+        println("Finished job fetching unplayable videos")
     }
 
     @ShellMethod("Generate report with all freeable Kaltura videos")
     fun freeableVideos(@ShellOption(help = "Please specify file name") filename: String) {
         println("Fetching all freeable videos...")
-        val playableVideos = videoAnalysisService.getFreeableVideos()
-        println("Generated file $filename with ${playableVideos.size} unplayable videos")
+        createReport.freeableVideos(filename)
+        println("Finished job fetching unplayable videos")
     }
 
     @ShellMethod("Count all videos")
     fun countVideos(@ShellOption(help = "Specify source (kaltura, boclips, all)", defaultValue = "all") source: String) {
-        if ("kaltura" == source.toLowerCase() || "all" == source.toLowerCase()) {
-            val allKalturaVideos = videoAnalysisService.countAllKalturaVideos()
-            println("Found $allKalturaVideos videos on Kaltura")
-        }
-        if ("boclips" == source.toLowerCase() || "all" == source.toLowerCase()) {
-            val allBoclipsVideos = videoAnalysisService.countAllBoclipsVideos()
-            println("Found $allBoclipsVideos videos in the Boclips database")
-        }
+        createReport.printCounts(source)
     }
 }
