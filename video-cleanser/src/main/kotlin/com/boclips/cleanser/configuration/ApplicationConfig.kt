3a24303b417831cfb07d3b0488ef5ec0f4ee50c6
Benjamin Gröhbiel <ben@boclips.com>
2018-06-22 15:34:00 -0500

    [#157550390] Add spring-retry to avoid media not being fetched
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

--- video-cleanser/src/main/kotlin/com/boclips/cleanser/configuration/ApplicationConfig.kt
+++ video-cleanser/src/main/kotlin/com/boclips/cleanser/configuration/ApplicationConfig.kt
@@ -2,13 +2,15 @@ package com.boclips.cleanser.configuration
 
 import com.boclips.cleanser.domain.service.VideoAnalysisService
 import com.boclips.cleanser.infrastructure.boclips.BoclipsVideoRepository
-import com.boclips.cleanser.infrastructure.kaltura.PaginationOrchestrator
 import com.boclips.cleanser.infrastructure.kaltura.PagedKalturaMediaService
+import com.boclips.cleanser.infrastructure.kaltura.PaginationOrchestrator
 import com.boclips.cleanser.infrastructure.kaltura.client.KalturaMediaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.retry.annotation.EnableRetry
 
 @Configuration
+@EnableRetry
 class ApplicationConfig {
     @Bean
     fun analysisService(boclipsVideoRepository: BoclipsVideoRepository,
