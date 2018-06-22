0cb64eb797aecb59a50241b21c78e861f3a1de91
Benjamin Gröhbiel <ben@boclips.com>
2018-06-22 08:48:34 -0500

    [#157550390] Add new shell command to dump all kaltura videos to file
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

--- video-cleanser/src/test/kotlin/com/boclips/cleanser/domain/service/VideoAnalysisServiceStagingE2E.kt
+++ video-cleanser/src/test/kotlin/com/boclips/cleanser/domain/service/VideoAnalysisServiceStagingE2E.kt
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-@ActiveProfiles("staging")
+@ActiveProfiles("staging-e2e")
 class VideoAnalysisServiceStagingE2E {
     @Autowired
     lateinit var videoAnalysisService: VideoAnalysisService
