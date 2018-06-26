4892126695b5cce32bcf119464571f99c9b49fcb
José Carlos Valero Sánchez <josecarlos@boclips.com>
2018-06-26 14:39:29 +0100

    [#157550390] Fix lastpass commands
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- video-analyser/setup-secrets.sh
+++ video-analyser/setup-secrets.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 lpass login $1
-lpass show --notes "video-videoanalyser application-staging.yml" > ./src/main/resources/application-staging.yml
-lpass show --notes "video-videoanalyser application-production.yml" > ./src/main/resources/application-production.yml
+lpass show --notes "video-analyser application-staging.yml" > ./src/main/resources/application-staging.yml
+lpass show --notes "video-analyser application-production.yml" > ./src/main/resources/application-production.yml
