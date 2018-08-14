b07589a82dab0fb634d091236a81c7d730da8c8b
Benjamin Gröhbiel <ben@boclips.com>
2018-08-14 17:28:07 +0100

    #159397908 no jokes, fix the path
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- video-service/pipeline/scripts/build_as_notroot.sh
+++ video-service/pipeline/scripts/build_as_notroot.sh
@@ -5,4 +5,4 @@ set -ex
 adduser --disabled-password notroot </dev/null
 usermod -aG root notroot
 
-su - notroot -c "$(pwd)/$(dirname $0)/build.sh"
+su - notroot -c "cd $(pwd) && $(dirname $0)/build.sh"
