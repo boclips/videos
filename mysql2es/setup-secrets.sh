371ba986a0abdfe934814fd0b5be200ecefcc597
Benjamin Gröhbiel <ben@boclips.com>
2018-08-17 15:18:03 +0100

    [#159397908] Add test for populating production

--- mysql2es/setup-secrets.sh
+++ mysql2es/setup-secrets.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-lpass login $1
 lpass show --notes "mysql2es application-staging.yml" > ./src/main/resources/application-staging.yml
 lpass show --notes "mysql2es application-production.yml" > ./src/main/resources/application-production.yml
--- mysql2es/src/test/kotlin/com/boclips/mysql2es/PopulateStaging.kt
+++ mysql2es/src/test/kotlin/com/boclips/mysql2es/PopulateStaging.kt
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(SpringRunner::class)
 @SpringBootTest
 @ActiveProfiles("staging")
-class Mysql2esApplicationTests {
+class PopulateStaging {
 
     @Autowired
     lateinit var migrationService: MigrationService
