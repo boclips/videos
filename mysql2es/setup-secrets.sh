3bc75b26812194d755de9573e07b56178380cf66
Benjamin Gröhbiel <ben@boclips.com>
2018-10-26 17:18:14 +0100

    Delete temporary es importer
    [#160818826]
    
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- mysql2es/setup-secrets.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-lpass show --notes "mysql2es application-staging.yml" > ./src/main/resources/application-staging.yml
-lpass show --notes "mysql2es application-production.yml" > ./src/main/resources/application-production.yml
