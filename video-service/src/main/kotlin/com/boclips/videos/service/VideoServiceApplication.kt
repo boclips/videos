b5da017944d41cf1301e400e12a82a31b21c662d
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2018-11-11 22:22:56 +0000

    Activate swagger API documentation
    Security is not yet integrated with the documentation.

--- video-service/src/main/kotlin/com/boclips/videos/service/VideoServiceApplication.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/VideoServiceApplication.kt
@@ -3,8 +3,10 @@ package com.boclips.videos.service
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @EnableAsync
+@EnableSwagger2
 @SpringBootApplication
 class VideoServiceApplication
 
