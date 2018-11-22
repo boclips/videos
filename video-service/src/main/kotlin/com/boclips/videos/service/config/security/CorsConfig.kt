8f33099367d02809d100011286875735923a25d1
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2018-11-22 22:51:53 +0000

    Revert "Remove CORS"
    This reverts commit 9de8636d34d2a1dac9371ed21feb91a96cdc5356.

--- video-service/src/main/kotlin/com/boclips/videos/service/config/security/CorsConfig.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/config/security/CorsConfig.kt
@@ -11,4 +11,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 @EnableBoclipsSecurity
 class WebSecurityConfig {
+    @Bean
+    fun corsConfigurer() = object : WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry.addMapping("/**")
+                    .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
+                    .allowedOrigins(
+                            "http://localhost:8081",
+                            "https://educators.staging-boclips.com",
+                            "https://educators.testing-boclips.com",
+                            "https://educators.boclips.com"
+                    )
+        }
+    }
 }
