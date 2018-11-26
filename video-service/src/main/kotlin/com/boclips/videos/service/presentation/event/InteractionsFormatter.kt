dc0a896801414a82f42060d6f046c8fd7120189a
Matt Jones <matt@boclips.com>
2018-11-26 15:06:37 +0000

    Format interaction with user origin
    [#162057207]
    
    Signed-off-by: José Carlos Valero Sánchez <josecarlos@boclips.com>

--- video-service/src/main/kotlin/com/boclips/videos/service/presentation/event/InteractionsFormatter.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/presentation/event/InteractionsFormatter.kt
@@ -1,13 +1,19 @@
 package com.boclips.videos.service.presentation.event
 
 import com.boclips.videos.service.infrastructure.event.analysis.Interaction
+import com.boclips.videos.service.infrastructure.event.types.User
 
 object InteractionsFormatter {
     fun format(interactions: List<Interaction>): String {
         return interactions.reversed()
-                .flatMap { listOf(">  ${format(it)}") + it.related.map { "   ${format(it)}" } }
+                .flatMap {
+                    listOf(">  ${format(it)} by ${format(it.user)}") +
+                            it.related.map { "   ${format(it)}" }
+                }
                 .joinToString("\n")
     }
+
+    private fun format(user: User) = if( user.boclipsEmployee ) "Boclips employee" else "teacher"
 
     private fun format(interaction: Interaction): String {
         return "${interaction.timestamp} ${interaction.description}"
--- video-service/src/test/kotlin/com/boclips/videos/service/presentation/event/InteractionsFormatterTest.kt
+++ video-service/src/test/kotlin/com/boclips/videos/service/presentation/event/InteractionsFormatterTest.kt
@@ -1,10 +1,9 @@
 package com.boclips.videos.service.presentation.event
 
-import com.boclips.videos.service.infrastructure.event.analysis.Interaction
+import com.boclips.videos.service.infrastructure.event.types.User
 import com.boclips.videos.service.testsupport.TestFactories.createInteraction
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 
@@ -14,17 +13,17 @@ class InteractionsFormatterTest {
     fun format() {
         val time = ZonedDateTime.of(2018, 2, 3, 10, 11, 12, 0, ZoneOffset.UTC)
         val interactions = listOf(
-                createInteraction(timestamp = time, description = "root", related = listOf(
+                createInteraction(timestamp = time, description = "root", user = User(false), related = listOf(
                         createInteraction(timestamp = time.plusHours(1), description = "nested", related = emptyList())
                 )),
-                createInteraction(timestamp = time.plusHours(2), description = "another", related = emptyList())
+                createInteraction(timestamp = time.plusHours(2), description = "another", user = User(true), related = emptyList())
         )
 
         val formatted = InteractionsFormatter.format(interactions)
 
         assertThat(formatted).isEqualTo(
-                """>  2018-02-03T12:11:12Z another
-                  |>  2018-02-03T10:11:12Z root
+                """>  2018-02-03T12:11:12Z another by Boclips employee
+                  |>  2018-02-03T10:11:12Z root by teacher
                   |   2018-02-03T11:11:12Z nested
                 """.trimMargin())
     }
