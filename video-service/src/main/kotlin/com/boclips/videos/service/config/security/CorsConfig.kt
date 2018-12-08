e65d66f1f20fe60052056600fef6dcdd56e150fc
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2018-12-08 18:33:45 +0000

    Move and test cors configuration to WebSecurityConfig
    [#162484754]

--- video-service/src/main/kotlin/com/boclips/videos/service/config/security/CorsConfig.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/config/security/CorsConfig.kt
@@ -1,27 +1,10 @@
 package com.boclips.videos.service.config.security
 
 import com.boclips.security.EnableBoclipsSecurity
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Profile("!test")
 @Configuration
 @EnableBoclipsSecurity
-class WebSecurityConfig {
-    @Bean
-    fun corsConfigurer() = object : WebMvcConfigurer {
-        override fun addCorsMappings(registry: CorsRegistry) {
-            registry.addMapping("/**")
-                    .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
-                    .allowedOrigins(
-                            "http://localhost:8081",
-                            "https://educators.staging-boclips.com",
-                            "https://educators.testing-boclips.com",
-                            "https://educators.boclips.com"
-                    )
-        }
-    }
-}
+class WebSecurityConfig
--- video-service/src/main/kotlin/com/boclips/videos/service/config/security/WebSecurityConfig.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/config/security/WebSecurityConfig.kt
@@ -4,11 +4,18 @@ import com.boclips.security.HttpSecurityConfigurer
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.stereotype.Component
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 
 @Component
 class VideoServiceHttpSecurityConfigurer : HttpSecurityConfigurer {
     override fun configure(http: HttpSecurity) {
+        http
+                .cors()
+                .configurationSource(corsConfiguration())
+
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
@@ -29,6 +36,21 @@ class VideoServiceHttpSecurityConfigurer : HttpSecurityConfigurer {
                 .antMatchers(HttpMethod.POST, "/v1/videos").hasRole(UserRoles.INSERT_VIDEOS)
                 .antMatchers(HttpMethod.GET, "/v1/videos*").hasAnyRole(UserRoles.TEACHER, UserRoles.VIEW_VIDEOS)
                 .antMatchers(HttpMethod.GET, "/v1/videos/*").permitAll()
+    }
+
+    private fun corsConfiguration(): CorsConfigurationSource? {
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("http://localhost:8081")
+        config.addAllowedOrigin("https://educators.staging-boclips.com")
+        config.addAllowedOrigin("https://educators.testing-boclips.com")
+        config.addAllowedOrigin("https://educators.boclips.com")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        return source
     }
 }
 
