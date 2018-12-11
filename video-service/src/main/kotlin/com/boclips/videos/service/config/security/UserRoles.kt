d2ad04f82557f6079a017976cd7f83d6e9762293
José Carlos Valero Sánchez <josecarlos@boclips.com>
2018-12-11 16:59:04 +0000

    Reformating

--- video-service/src/main/kotlin/com/boclips/videos/service/config/security/UserRoles.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/config/security/UserRoles.kt
@@ -3,8 +3,10 @@ package com.boclips.videos.service.config.security
 object UserRoles {
     const val VIEW_EVENTS = "VIEW_EVENTS"
     const val INSERT_EVENTS = "INSERT_EVENTS"
+
     const val REMOVE_VIDEOS = "REMOVE_VIDEOS"
     const val VIEW_VIDEOS = "VIEW_VIDEOS"
     const val INSERT_VIDEOS = "INSERT_VIDEOS"
+
     const val REBUILD_SEARCH_INDEX = "REBUILD_SEARCH_INDEX"
 }
