67472a66818e197cc1cfc3e9c8c8314505244c02
Jacek Rzeniewicz <jacek@rzrz.pl>
2018-12-18 10:52:59 +0100

    Revert "Ignore unknown properties in video/links resource"
    This reverts commit 4cad797b06914cc0a6c820a16b573a5a440e6dfe.

--- video-service/src/client/java/com/boclips/videos/service/client/internal/resources/LinksResource.java
+++ video-service/src/client/java/com/boclips/videos/service/client/internal/resources/LinksResource.java
@@ -1,10 +1,8 @@
 package com.boclips.videos.service.client.internal.resources;
 
 import lombok.Data;
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class LinksResource {
     private Links _links;
 }
