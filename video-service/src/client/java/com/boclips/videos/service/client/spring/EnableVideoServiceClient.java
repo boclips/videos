e305589e0f444b6f15596a032db6affd81e65ac1
Jacek Rzeniewicz <jacek@boclips.com>
2018-12-19 18:28:05 +0100

    Make @EnableVideoServiceClient public
    [#162410795]

--- video-service/src/client/java/com/boclips/videos/service/client/spring/EnableVideoServiceClient.java
+++ video-service/src/client/java/com/boclips/videos/service/client/spring/EnableVideoServiceClient.java
@@ -10,6 +10,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Import(VideoServiceClientConfig.class)
-@interface EnableVideoServiceClient {
+public @interface EnableVideoServiceClient {
 
 }
