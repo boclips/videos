eb7ad031c3185690985337fc0eca4ed9f324545f
Andrew Bruce <me@andrewbruce.net>
2019-01-08 12:03:58 +0000

    Assert on optional category param
    [#162643267]
    
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

--- video-service/src/test/kotlin/com/boclips/videos/service/presentation/LinksControllerTest.kt
+++ video-service/src/test/kotlin/com/boclips/videos/service/presentation/LinksControllerTest.kt
@@ -22,6 +22,7 @@ class LinksControllerTest : AbstractSpringIntegrationTest() {
         mockMvc.perform(get("/v1"))
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("$._links.search.href", containsString("/videos?query=")))
+                .andExpect(jsonPath("$._links.search.href", containsString("{&category}")))
                 .andExpect(jsonPath("$._links.search.templated", equalTo(true)))
                 .andExpect(jsonPath("$._links.video.href", containsString("/videos/")))
                 .andExpect(jsonPath("$._links.video.templated", equalTo(true)))
