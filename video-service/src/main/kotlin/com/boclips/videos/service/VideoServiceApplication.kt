5dc7a5c3513b9dab5c862120679dbafa2c56f0db
Andrew Bruce <me@andrewbruce.net>
2019-01-10 13:56:43 +0000

    Exclude Solr Spring autoconfig
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- video-service/src/main/kotlin/com/boclips/videos/service/VideoServiceApplication.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/VideoServiceApplication.kt
@@ -1,13 +1,14 @@
 package com.boclips.videos.service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @EnableAsync
 @EnableSwagger2
-@SpringBootApplication
+@SpringBootApplication(exclude = [SolrAutoConfiguration::class])
 class VideoServiceApplication
 
 fun main(args: Array<String>) {
