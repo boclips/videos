3141bb71b0fc63f49271b91f20443bb9bd4f7949
Oli Peate <oli@boclips.com>
2019-01-14 12:26:13 +0000

    Test Video Repository streams are closed
    Retro-fits test to protect the `use` block behaviour, which was
    previously removed by mistake then re-instated.
    
    [#163019772]

--- video-service/src/test/kotlin/com/boclips/videos/service/infrastructure/video/MysqlVideoAssetRepositoryTest.kt
+++ video-service/src/test/kotlin/com/boclips/videos/service/infrastructure/video/MysqlVideoAssetRepositoryTest.kt
@@ -13,10 +13,9 @@ class MysqlVideoAssetRepositoryTest {
 
     @Test
     fun `streamAll ignores invalid assets`() {
-
         val subjectRepository = mock<SubjectRepository>()
 
-        val videoEntityRepository = mock<VideoEntityRepository>() {
+        val videoEntityRepository = mock<VideoEntityRepository> {
             on { readAll() } doReturn Stream.of(
                     TestFactories.createVideoEntity(title = "corrupt item").apply { playback_id = null },
                     TestFactories.createVideoEntity(title = "this one is ok")
@@ -28,5 +27,30 @@ class MysqlVideoAssetRepositoryTest {
         repository.streamAll { assets = it.toList() }
 
         assertThat(assets.map { it.title }).containsExactly("this one is ok")
+    }
+
+    @Test
+    fun `streamAll closes the stream after the use`() {
+        var closeCalled = false
+        val subjectRepository = mock<SubjectRepository>()
+
+        val videoEntityRepository = mock<VideoEntityRepository> {
+            on { readAll() } doReturn Stream.of(
+                    TestFactories.createVideoEntity()
+            ).onClose {
+                closeCalled = true
+            }
+        }
+
+        val repository = MysqlVideoAssetRepository(subjectRepository, videoEntityRepository)
+        repository.streamAll {  }
+
+        assertThat(closeCalled)
+                .overridingErrorMessage("""
+                    Expected stream to be closed but it wasn't.
+                    In kotlin we should apply `use` (https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.io/use.html),
+                    in place of Java's `try-with-resources` (https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html)
+                """)
+                .isTrue()
     }
 }
