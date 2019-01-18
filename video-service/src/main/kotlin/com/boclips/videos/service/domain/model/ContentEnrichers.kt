3dd4926b49f394a153d3e6db6901f2963a1ff99b
Oli Peate <oli@boclips.com>
2019-01-18 11:31:32 +0000

    Update definition of non-classroom content
    [#162982716]

--- video-service/src/main/kotlin/com/boclips/videos/service/domain/model/ContentEnrichers.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/domain/model/ContentEnrichers.kt
@@ -6,19 +6,16 @@ import com.boclips.videos.service.domain.model.asset.LegacyVideoType
 class ContentEnrichers {
     companion object {
         fun isClassroom(videoAsset: VideoAsset): Boolean {
-            val lowercaseTextFragments = listOf(videoAsset.title, videoAsset.description).map { it.toLowerCase() }
+            val lowercaseTextFragments = listOf(videoAsset.title, videoAsset.description).map(String::toLowerCase)
             val lowercaseText = lowercaseTextFragments.joinToString("\n")
             val bagOfWords = lowercaseTextFragments.flatMap { it.split(" ") }.toSet()
 
             return when {
                 videoAsset.type != LegacyVideoType.STOCK -> true
-                bagOfWords.contains("awards") -> false
-                bagOfWords.contains("premiere") -> false
-                lowercaseText.contains("red carpet") -> false
-                bagOfWords.contains("speech") -> true
-                lowercaseText.contains("archive public information film") -> true
-                bagOfWords.containsAll(listOf("biology", "animation")) -> true
-                bagOfWords.containsAll(listOf("space", "animation")) -> true
+                classroomExcludedWords.any { bagOfWords.contains(it) } -> false
+                classroomExcludedPhrases.any { lowercaseText.contains(it) } -> false
+                classroomPermittedPhrases.any { lowercaseText.contains(it) } -> true
+                classroomPermittedNonAdjacentWords.any { bagOfWords.containsAll(it) } -> true
                 else -> false
             }
         }
@@ -26,6 +23,38 @@ class ContentEnrichers {
         fun isNews(videoAsset: VideoAsset): Boolean {
             return videoAsset.type == LegacyVideoType.NEWS
         }
+
+        private val classroomExcludedWords = listOf(
+                "award",
+                "awards",
+                "cannes",
+                "celebrities",
+                "celebrity",
+                "exchange",
+                "hollywood",
+                "naked",
+                "nude",
+                "party",
+                "premier",
+                "premiere",
+                "sexy",
+                "topless"
+        )
+
+        private val classroomExcludedPhrases = listOf(
+                "red carpet",
+                "los angeles"
+        )
+
+        private val classroomPermittedPhrases = listOf(
+                "archive public information film"
+        )
+
+        private val classroomPermittedNonAdjacentWords = listOf(
+                listOf("speech"),
+                listOf("biology", "animation"),
+                listOf("space", "animation")
+        )
     }
 }
 
--- video-service/src/test/kotlin/com/boclips/videos/service/domain/model/ContentEnrichersTest.kt
+++ video-service/src/test/kotlin/com/boclips/videos/service/domain/model/ContentEnrichersTest.kt
@@ -1,5 +1,6 @@
 package com.boclips.videos.service.domain.model
 
+import com.boclips.videos.service.domain.model.ContentEnrichers.Companion.isClassroom
 import com.boclips.videos.service.domain.model.asset.LegacyVideoType
 import com.boclips.videos.service.testsupport.TestFactories.createVideoAsset
 import org.assertj.core.api.Assertions.assertThat
@@ -10,50 +11,70 @@ class ContentEnrichersTest {
 
     @Test
     fun `non stock content is matched as classroom`() {
-        val video = createVideoAsset(type = LegacyVideoType.INSTRUCTIONAL_CLIPS)
-
-        assertThat(ContentEnrichers.isClassroom(video)).isTrue()
+        assertThat(isClassroom(createVideoAsset(type = LegacyVideoType.INSTRUCTIONAL_CLIPS))).isTrue()
     }
 
     @Test
     fun `stock content with the word "speech" is matched as classroom`() {
-        assertThat(ContentEnrichers.isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "jfk state of union speech"))).isTrue()
-        assertThat(ContentEnrichers.isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, description = "jfk state of union Speech"))).isTrue()
+        assertThat(isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "jfk state of union speech"))).isTrue()
+        assertThat(isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, description = "jfk state of union Speech"))).isTrue()
     }
 
     @Test
     fun `stock content without the word "speech" is NOT matched as classroom`() {
-        assertThat(ContentEnrichers.isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "Family left speechless by bride's wedding dress"))).isFalse()
+        assertThat(isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "Family left speechless by bride's wedding dress"))).isFalse()
     }
 
     @Test
     fun `stock content with the phrase "archive public information film" is matched as classroom`() {
-        assertThat(ContentEnrichers.isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "Archive public information film: Woman throwing grain (to chickens)"))).isTrue()
+        assertThat(isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "Archive public information film: Woman throwing grain (to chickens)"))).isTrue()
     }
 
     @Test
     fun `stock content containing words "biology" and "animation" is matched as classroom`() {
-        assertThat(ContentEnrichers.isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "Animation explaining all about biology"))).isTrue()
+        assertThat(isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "Animation explaining all about biology"))).isTrue()
     }
 
     @Test
     fun `stock content containing words "space" and "animation" is matched as classroom`() {
-        assertThat(ContentEnrichers.isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "Animation explaining all about space"))).isTrue()
+        assertThat(isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "Animation explaining all about space"))).isTrue()
     }
 
     @Test
-    fun `stock content containing the word "awards" is NOT matched as classroom`() {
-        assertThat(ContentEnrichers.isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "speech archive public information film biology space animation awards"))).isFalse()
+    fun `stock featuring any of excluded words is NOT matched as classroom`() {
+        val excluded = listOf(
+                "award",
+                "awards",
+                "Cannes",
+                "celebrities",
+                "celebrity",
+                "exchange",
+                "hollywood",
+                "naked",
+                "nude",
+                "party",
+                "premier",
+                "premiere",
+                "sexy",
+                "topless"
+        )
+
+        excluded.forEach {
+            val title = "biology space animation $it"
+            assertThat(isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = title))).withFailMessage("Expected '$title' to be excluded").isFalse()
+        }
     }
 
     @Test
-    fun `stock content containing the word "premiere" is NOT matched as classroom`() {
-        assertThat(ContentEnrichers.isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "speech archive public information film biology space animation premiere"))).isFalse()
-    }
+    fun `stock featuring the excluded phrases is NOT matched as classroom`() {
+        val excluded = listOf(
+                "red carpet",
+                "Los Angeles"
+        )
 
-    @Test
-    fun `stock content containing the phrase "red carpet" is NOT matched as classroom`() {
-        assertThat(ContentEnrichers.isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "speech archive public information film biology space animation red carpet"))).isFalse()
-        assertThat(ContentEnrichers.isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = "speech archive public information film biology space animation red colour carpet"))).isTrue()
+        excluded.forEach {
+            val title = "biology space animation $it"
+            assertThat(isClassroom(createVideoAsset(type = LegacyVideoType.STOCK, title = title))).withFailMessage("Expected '$title' to be excluded").isFalse()
+        }
     }
 }
