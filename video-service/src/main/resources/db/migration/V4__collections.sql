ceadb8145f9dd59cb4ef207b56ba86afda4bbadf
Matt Jones <matt@boclips.com>
2019-01-18 10:35:29 +0000

    Fix migration syntax
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- video-service/src/main/resources/db/migration/V4__collections.sql
+++ video-service/src/main/resources/db/migration/V4__collections.sql
@@ -11,5 +11,5 @@ CREATE TABLE IF NOT EXISTS collection_video
   collection_id VARCHAR(64) NOT NULL,
   video_id INT NOT NULL,
   FOREIGN KEY (collection_id) REFERENCES collection(id) ON DELETE CASCADE,
-  FOREIGN KEY (video_id) REFERENCES metadata_orig(id) ON DELETE CASCADE,
+  FOREIGN KEY (video_id) REFERENCES metadata_orig(id) ON DELETE CASCADE
 );
