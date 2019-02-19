6fc1c3448d0e65e6ffe048acd48f27d210234c7c
Oli Peate <oli@boclips.com>
2019-02-19 14:47:59 +0000

    Make CollectionResource props concrete

--- video-service/src/main/kotlin/com/boclips/videos/service/presentation/collections/CollectionResource.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/presentation/collections/CollectionResource.kt
@@ -5,8 +5,8 @@ import org.springframework.hateoas.core.Relation
 
 @Relation(collectionRelation = "collections")
 data class CollectionResource(
-    val id: String? = null,
-    val owner: String? = null,
-    val title: String? = null,
-    val videos: List<VideoResource>? = null
+    val id: String,
+    val owner: String,
+    val title: String,
+    val videos: List<VideoResource>
 )
