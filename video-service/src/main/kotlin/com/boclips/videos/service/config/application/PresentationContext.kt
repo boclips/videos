73d6fec7bcc0bc7b25503f6798acec2fcf16adb0
Jacek Rzeniewicz <jacek@boclips.com>
2019-03-15 14:01:00 +0000

    Upgrade to Spring Boot 2.1.3

---
+++ video-service/src/main/kotlin/com/boclips/videos/service/config/application/PresentationContext.kt
@@ -0,0 +1,17 @@
+package com.boclips.videos.service.config.application
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.ForwardedHeaderFilter
+
+@Configuration
+class PresentationContext {
+
+    @Bean
+    fun forwardedHeaderFilter(): FilterRegistrationBean<ForwardedHeaderFilter> {
+        val filter = FilterRegistrationBean<ForwardedHeaderFilter>()
+        filter.filter = ForwardedHeaderFilter()
+        return filter
+    }
+}
