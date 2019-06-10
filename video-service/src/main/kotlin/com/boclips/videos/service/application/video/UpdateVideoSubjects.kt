fb2a633777d0753374361eb45b5b0c39854af31a
Jacek Rzeniewicz <jacek@boclips.com>
2019-06-10 12:30:56 +0100

    Add logging around updating video subjects

--- video-service/src/main/kotlin/com/boclips/videos/service/application/video/UpdateVideoSubjects.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/application/video/UpdateVideoSubjects.kt
@@ -6,18 +6,26 @@ import com.boclips.videos.service.domain.model.video.VideoId
 import com.boclips.videos.service.domain.model.video.VideoRepository
 import com.boclips.videos.service.domain.service.subject.SubjectRepository
 import com.boclips.videos.service.domain.service.video.VideoUpdateCommand
+import mu.KLogging
 import org.springframework.cloud.stream.annotation.StreamListener
+import java.lang.Exception
 
 class UpdateVideoSubjects(
         private val videoRepository: VideoRepository,
         private val subjectRepository: SubjectRepository
 ) {
+    companion object : KLogging()
 
     @StreamListener(Subscriptions.VIDEO_SUBJECT_CLASSIFIED)
     operator fun invoke(videoSubjectClassified: VideoSubjectClassified) {
         val videoId = VideoId(videoSubjectClassified.videoId)
-        val subjects = subjectRepository.findByIds(videoSubjectClassified.subjects.map { it.id })
-        val updateCommand = VideoUpdateCommand.ReplaceSubjects(videoId, subjects)
-        videoRepository.update(updateCommand)
+        try {
+            val subjects = subjectRepository.findByIds(videoSubjectClassified.subjects.map { it.id })
+            val updateCommand = VideoUpdateCommand.ReplaceSubjects(videoId, subjects)
+            videoRepository.update(updateCommand)
+            logger.info { "Updates subjects of video ${videoId.value}: ${subjects.joinToString(", ") { it.name }}" }
+        } catch(e: Exception) {
+            logger.error(e) { "Updating subjects of video ${videoId.value} failed and will not be retried" }
+        }
     }
 }
