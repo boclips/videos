330a00cfc31cdcb2840a9191188b2b92575a0dae
Rebecca Appleyard <rebecca@boclips.com>
2019-06-26 16:39:40 +0100

    Reinstate label for AgeRangeResource
    [#165680586]

--- video-service/src/main/kotlin/com/boclips/videos/service/presentation/ageRange/AgeRangeResource.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/presentation/ageRange/AgeRangeResource.kt
@@ -3,4 +3,6 @@ package com.boclips.videos.service.presentation.ageRange
 data class AgeRangeResource(
     val min: Int?,
     val max: Int?
-)
+) {
+    fun getLabel() = min?.let { max?.let { "$min-$max" } ?: "$min+" } ?: ""
+}
