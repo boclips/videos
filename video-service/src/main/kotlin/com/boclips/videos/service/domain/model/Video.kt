ba1e9543e4e16b06e187914fdd7f7dfad7360bc8
Matt Jones <matt@boclips.com>
2019-07-03 13:56:57 +0100

    Remove custom equals and hashcode on Video
    This was added by Ben, and he said it was OK to completely remove this
    code. So ask him.
    
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- video-service/src/main/kotlin/com/boclips/videos/service/domain/model/Video.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/domain/model/Video.kt
@@ -41,19 +41,4 @@ data class Video(
     override fun toString(): String {
         return "Video(videoId=$videoId, title='$title', contentPartnerName='${contentPartner.name}')"
     }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Video
-
-        if (videoId != other.videoId) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return videoId.hashCode()
-    }
 }
