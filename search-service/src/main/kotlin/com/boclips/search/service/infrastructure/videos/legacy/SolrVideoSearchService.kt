bafeb51dfb42fb21d901cde98f552aa5b0678198
Rafal Lewandowski <rafal@boclips.com>
2019-09-24 11:07:11 +0100

    Increase upsert batch size for solr
    [- This is a fix for this Sentry issue: https://sentry.io/organizations/boclips/issues/1224149862/?environment=production&project=1402873&statsPeriod=14d]
    
    Signed-off-by: Rebecca Appleyard <rebecca@boclips.com>

--- search-service/src/main/kotlin/com/boclips/search/service/infrastructure/videos/legacy/SolrVideoSearchService.kt
+++ search-service/src/main/kotlin/com/boclips/search/service/infrastructure/videos/legacy/SolrVideoSearchService.kt
@@ -12,7 +12,7 @@ import org.apache.solr.client.solrj.impl.HttpSolrClient
 
 class SolrVideoSearchService(host: String, port: Int) : LegacyVideoSearchService {
     companion object : KLogging() {
-        private const val UPSERT_BATCH_SIZE = 2000
+        private const val UPSERT_BATCH_SIZE = 4000
     }
 
     override fun safeRebuildIndex(items: Sequence<LegacyVideoMetadata>, notifier: ProgressNotifier?) {
