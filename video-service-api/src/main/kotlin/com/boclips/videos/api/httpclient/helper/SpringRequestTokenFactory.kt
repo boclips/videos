8b55e4f0da8ff0f6e61fe333486e1ae5812120b2
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2020-01-03 00:01:33 +0000

    Clean up gradle dependencies in video-service-client

--- video-service-api/src/main/kotlin/com/boclips/videos/api/httpclient/helper/SpringRequestTokenFactory.kt
+++ video-service-api/src/main/kotlin/com/boclips/videos/api/httpclient/helper/SpringRequestTokenFactory.kt
@@ -5,7 +5,7 @@ import org.springframework.web.context.request.ServletRequestAttributes
 
 class SpringRequestTokenFactory : TokenFactory {
     override fun getAccessToken(): String {
-        return RequestContextHolder.getRequestAttributes().let {
+        RequestContextHolder.getRequestAttributes().let {
             return when (it) {
                 is ServletRequestAttributes -> it.request.getHeader("Authorization").orEmpty().removePrefix("Bearer ")
                 else -> ""
