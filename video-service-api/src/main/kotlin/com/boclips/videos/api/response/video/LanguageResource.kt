491b8c61f8ee2b7304153b3e9fcb5d230dab9813
Matt Jones <matt@boclips.com>
2020-01-27 11:37:57 +0000

    Use displayLanguage when converting language
    [#169457341]

--- video-service-api/src/main/kotlin/com/boclips/videos/api/response/video/LanguageResource.kt
+++ video-service-api/src/main/kotlin/com/boclips/videos/api/response/video/LanguageResource.kt
@@ -4,6 +4,6 @@ import java.util.Locale
 
 data class LanguageResource(val code: String, val displayName: String) {
     companion object {
-        fun from(locale: Locale) = LanguageResource(code = locale.isO3Language, displayName = locale.displayName)
+        fun from(locale: Locale) = LanguageResource(code = locale.isO3Language, displayName = locale.displayLanguage)
     }
 }
