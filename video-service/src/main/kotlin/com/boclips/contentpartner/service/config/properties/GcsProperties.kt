e73e72abeafd408165828c686268958a5efc5247
Sebastian Kazenbroot-Guppy <sebastian@boclips.com>
2020-02-19 17:18:52 +0000

    Make bucketName configuration item settable
    [#170801308]

--- video-service/src/main/kotlin/com/boclips/contentpartner/service/config/properties/GcsProperties.kt
+++ video-service/src/main/kotlin/com/boclips/contentpartner/service/config/properties/GcsProperties.kt
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Component
 data class GcsProperties(
     var projectId: String = "",
     var secret: String = "",
-    val bucketName: String = ""
-    )
+    var bucketName: String = ""
+)
