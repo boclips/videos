74068b486dde7d7dbc10d7b307e8f36051f5650d
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2020-02-27 19:17:38 +0000

    Move tests for permitted/denied ids in same place

--- search-service/src/test/kotlin/com/boclips/search/service/infrastructure/videos/VideoIndexReaderIdsIntegrationTest.kt
+++ search-service/src/test/kotlin/com/boclips/search/service/infrastructure/videos/VideoIndexReaderIdsIntegrationTest.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-internal class VideoIndexReaderPermittedIdsIntegrationTest : EmbeddedElasticSearchIntegrationTest() {
+internal class VideoIndexReaderIdsIntegrationTest : EmbeddedElasticSearchIntegrationTest() {
     private lateinit var videoIndexReader: VideoIndexReader
     private lateinit var videoIndexWriter: VideoIndexWriter
 
@@ -21,7 +21,7 @@ internal class VideoIndexReaderPermittedIdsIntegrationTest : EmbeddedElasticSear
     }
 
     @Nested
-    inner class BySearchQuery {
+    inner class PermittedIds {
         @Test
         fun `limits search when permitted ids are specified`() {
             videoIndexWriter.upsert(
@@ -91,6 +91,63 @@ internal class VideoIndexReaderPermittedIdsIntegrationTest : EmbeddedElasticSear
             )
 
             assertThat(results).containsExactlyInAnyOrder("1", "2")
+        }
+    }
+
+    @Nested
+    inner class DeniedIds {
+        @Test
+        fun `denied videos are omitted by search`() {
+            videoIndexWriter.upsert(
+                sequenceOf(
+                    SearchableVideoMetadataFactory.create(id = "1", title = "apple"),
+                    SearchableVideoMetadataFactory.create(id = "2", title = "apple & bananas")
+                )
+            )
+
+            val results = videoIndexReader.search(
+                PaginatedSearchRequest(query = VideoQuery(deniedVideoIds = setOf("1"), phrase = "apple"))
+            )
+
+            assertThat(results).containsExactly("2")
+        }
+
+        @Test
+        fun `denied videos are omitted by search, and override permitted ids`() {
+            videoIndexWriter.upsert(
+                sequenceOf(
+                    SearchableVideoMetadataFactory.create(id = "1", title = "apple"),
+                    SearchableVideoMetadataFactory.create(id = "2", title = "apple & bananas")
+                )
+            )
+
+            val results = videoIndexReader.search(
+                PaginatedSearchRequest(
+                    query = VideoQuery(
+                        deniedVideoIds = setOf("1"),
+                        permittedVideoIds = setOf("1"),
+                        phrase = "apple"
+                    )
+                )
+            )
+
+            assertThat(results).containsExactly("2")
+        }
+
+        @Test
+        fun `denied videos not matching any ids are ignored`() {
+            videoIndexWriter.upsert(
+                sequenceOf(
+                    SearchableVideoMetadataFactory.create(id = "1", title = "apple"),
+                    SearchableVideoMetadataFactory.create(id = "2", title = "apple & bananas")
+                )
+            )
+
+            val results = videoIndexReader.search(
+                PaginatedSearchRequest(query = VideoQuery(deniedVideoIds = setOf("100"), phrase = "apple"))
+            )
+
+            assertThat(results).containsExactly("1", "2")
         }
     }
 }
