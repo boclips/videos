497d1a7e77e22616b184a019b0aacff89e8db2c2
José Carlos Valero Sánchez <josecarlos@boclips.com>
2020-06-02 10:22:44 +0100

    Tweak async task executor to support long-lived connections
    [#171813307]

--- video-service/src/main/kotlin/com/boclips/videos/service/config/WebConfig.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/config/WebConfig.kt
@@ -7,17 +7,24 @@ import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.module.SimpleModule
+import mu.KLogging
 import org.springframework.context.annotation.Configuration
 import org.springframework.hateoas.MediaTypes.HAL_JSON
 import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import java.time.Duration
+import java.util.concurrent.Callable
 
 @Configuration
 @EnableBoclipsApiErrors
 class WebConfig : WebMvcConfigurer {
+    companion object : KLogging()
     /*
         This snippet configures Spring MVC message converters that deal with JSON serialization (using jackson).
 
@@ -40,6 +47,26 @@ class WebConfig : WebMvcConfigurer {
                 it.objectMapper.registerModule(customizations)
             }
         }
+    }
+
+    /*
+    Async support is utilised for StreamingResponseBody which is used to download zip assets piped
+    from good'ol kaltura.
+     */
+    override fun configureAsyncSupport(configurer: AsyncSupportConfigurer) {
+        configurer
+            .setTaskExecutor(ThreadPoolTaskExecutor().apply {
+                corePoolSize = 5
+                maxPoolSize = 10
+                setQueueCapacity(25)
+            })
+            .setDefaultTimeout(18000)
+            .registerCallableInterceptors(object : TimeoutCallableProcessingInterceptor() {
+                override fun <T> handleTimeout(request: NativeWebRequest?, task: Callable<T>?): Any? {
+                    logger.error("Timeout during async processing: Boost timeout or reconsider strategy")
+                    return super.handleTimeout(request, task)
+                }
+            })
     }
 }
 
