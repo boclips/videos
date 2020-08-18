b92fb58da902f73dc9ff48137e371393d14a2019
Boisababy Clips <bob@fleming.nowhere>
2020-08-18 15:09:56 +0100

    Revert "Anonym video access without shareCode"
    This reverts commit 07c1eb19fa605cc95136f22402a504dbe8646340.

--- video-service/src/main/kotlin/com/boclips/videos/service/presentation/VideoController.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/presentation/VideoController.kt
@@ -160,7 +160,7 @@ class VideoController(
 
         val canAccessProtectedAttributes = when {
             getCurrentUser().isAuthenticated -> true
-            shareCode == null || referer == null -> false
+            shareCode == null || referer == null -> true
             else -> userService.isShareCodeValid(referer, shareCode)
         }
 
