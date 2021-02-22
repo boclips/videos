245ff0c2e993a94bf18c118a3d146b95130b81ad
Alex Glen <alex@boclips.com>
2021-02-22 10:19:22 +0000

    Add logging to get video price
    [#176945059]

--- video-service/src/main/kotlin/com/boclips/videos/service/application/video/search/GetVideoPrice.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/application/video/search/GetVideoPrice.kt
@@ -2,22 +2,26 @@ package com.boclips.videos.service.application.video.search
 
 import com.boclips.videos.service.domain.model.video.*
 import com.boclips.videos.service.domain.service.user.UserService
+import mu.KLogging
 
 class GetVideoPrice(
     private val userService: UserService,
     private val priceComputingService: PriceComputingService
 ) {
+
+    companion object : KLogging()
+
     operator fun invoke(video: BaseVideo, userId: String?): Price? {
         val videoPrices = userId?.let { userService.getOrganisationOfUser(it)?.deal?.prices }
 
-        return priceComputingService.computeVideoPrice(
+        val price =  priceComputingService.computeVideoPrice(
             videoId = video.videoId,
             organisationPrices = videoPrices,
             channel = video.channel.channelId,
             playback = video.playback,
             videoTypes = video.types
         )
+        logger.info { "calculated price of ${video.videoId.value} - ${price?.amount}" }
+        return price
     }
-
-
 }
