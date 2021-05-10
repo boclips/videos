2a631c0410cd83aa8600293237ff1c7e8778785e
Alex Glen <alex@boclips.com>
2021-05-10 21:06:48 +0100

    Add explode modifier to ingestType query param
    [#177911593]

--- video-service/src/main/kotlin/com/boclips/contentpartner/service/presentation/hateoas/ChannelLinkBuilder.kt
+++ video-service/src/main/kotlin/com/boclips/contentpartner/service/presentation/hateoas/ChannelLinkBuilder.kt
@@ -39,7 +39,7 @@ class ChannelLinkBuilder(private val uriComponentsBuilderFactory: UriComponentsB
                 getChannelsRoot()
                     .build()
                     .toUriString()
-                    .plus("{?name,projection,ingestType}"),
+                    .plus("{?name,projection,ingestType*}"),
                 Rels.CHANNELS
             )
         }
