14a6e668ebb3ff1f297a6a1ba3310f2a01d83849
Marcin Janik <marcin@boclips.com>
2021-05-13 17:46:08 +0200

    Make fields optional in VideoCategoriesDocument
    -optional fields are correctly handled in converter

--- video-service/src/main/kotlin/com/boclips/videos/service/infrastructure/video/VideoCategoriesDocument.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/infrastructure/video/VideoCategoriesDocument.kt
@@ -3,6 +3,6 @@ package com.boclips.videos.service.infrastructure.video
 import com.boclips.videos.service.infrastructure.taxonomy.CategoryWithAncestorsDocument
 
 data class VideoCategoriesDocument(
-    val channel: Set<CategoryWithAncestorsDocument>,
-    val manual: Set<CategoryWithAncestorsDocument>,
+    val channel: Set<CategoryWithAncestorsDocument>?,
+    val manual: Set<CategoryWithAncestorsDocument>?,
 )
