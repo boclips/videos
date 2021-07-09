d2bb8d2a8f37ee8661dc4a819228e7623383ed24
Mima Farley-Rose <mima@boclips.com>
2021-07-09 13:49:42 +0200

    Add setUpdatedAt migration script
    [#178621784]
    Signed-off-by: Marcin Janik <marcin@boclips.com>

--- video-service/src/main/kotlin/com/boclips/videos/service/infrastructure/video/VideoCollectionChangeLog.kt
+++ video-service/src/main/kotlin/com/boclips/videos/service/infrastructure/video/VideoCollectionChangeLog.kt
@@ -25,4 +25,27 @@ class VideoCollectionChangeLog {
 
         logger.info { "unsetCategories results: $updateResult" }
     }
+
+    @ChangeSet(order = "002", id = "2021-07-09T12:15", author = "mfarleyrose & mjanik")
+    fun setUpdatedAt(
+        @NonLockGuarded mongoClient: MongoClient,
+    ) {
+        // so we've run it by Robo because current mongo driver does not support $toDate but I'm leaving this in here
+        // because this logic could be useful in the future
+        // db.getCollection('videos').aggregate([
+        //    {$match: {"updatedAt": null}},
+        //    {$addFields: {"updatedAt": {$toString: {$toDate: "$_id"}}}},
+        //    {$merge: "videos"}
+        // ])
+        //
+        // the equivalent in mongo (if we wanted to copy from ingestedAt) would be:
+        // mongoClient.getDatabase(DATABASE_NAME).getCollection("videos")
+        //            .aggregate(
+        //                listOf(
+        //                    Aggregates.match(Filters.eq("updatedAt", null)),
+        //                    Aggregates.addFields(Field("updatedAt", "\$ingestedAt")),
+        //                    Aggregates.merge("videos")
+        //                )
+        //            ).toList() 
+    }
 }
